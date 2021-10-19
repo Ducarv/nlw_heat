1fe3018020bd0daafbef0e69a2e131f32650168f
Eduardo <educarvalho009@gmail.com>
2021-10-19 09:46:40 -0400

    nlw stage 1 end

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import "dotenv/config";
 import express from "express";
+import http from "http";
+import cors from "cors"
+import { Server } from "socket.io";
 import { router } from "./routes";
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
 
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`Usuario conectado no socket ${socket.id}`)
+})
+
+app.use(express.json());
 app.use(router);
 
 app.get("/github", (request, response) => {
@@ -21,4 +36,4 @@ app.get("/signin/callback", (request, response) => {
     return response.json(code)
 })
 
-app.listen(4000, () => console.log(`Server is running🚀`));
+export { serverHttp, io };
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./Controllers/AuthenticateUserController";
+import { CreateMessageController } from "./Controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./Controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./Controllers/ProfileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
 
 export { router };
